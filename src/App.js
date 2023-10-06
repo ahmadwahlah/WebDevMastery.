d034460fff920fff10a9464a12ac45ea145d8cc7
ahmadwahlah <ahmadwahlah@gmail.com>
2023-10-06 23:51:03 +0500

    newsletter component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Dashboard from "./components/Dashboard";
+import Newsletter from "./components/Newsletter";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Navbar />
         <Hero />
         <Dashboard />
+        <Newsletter />
       </div>
     </div>
   );
