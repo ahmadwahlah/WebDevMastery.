0fa223e2bbe89613deef77d66e6bbd1bca43f674
ahmadwahlah <ahmadwahlah@gmail.com>
2023-10-06 16:03:25 +0500

    modified readme and dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,17 +4,23 @@ import Laptop from "../assets/laptop.jpg";
 function Dashboard() {
   return (
     <div className=" w-full bg-white py-16 px-4">
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2"></div>
-      <img src={Laptop} alt="/" />
-      <div>
-        <p>WEB DEVELOPMENT DASHBOARD</p>
-        <h1>Streamline Your Web Projects</h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta beatae
-          omnis obcaecati aliquam, quisquam et eligendi officia atque, quam
-          corrupti consequatur commodi mollitia non, assumenda aliquid hic at
-          possimus nihil?
-        </p>
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 ">
+        <img className=" w-[500px] mx-auto my-4" src={Laptop} alt="/" />
+        <div className="flex flex-col justify-center">
+          <p className="text-[#00df9a] font-bold">WEB DEVELOPMENT DASHBOARD</p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Streamline Your Web Projects
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
+            beatae omnis obcaecati aliquam, quisquam et eligendi officia atque,
+            quam corrupti consequatur commodi mollitia non, assumenda aliquid
+            hic at possimus nihil?
+          </p>
+          <button className=" hover:bg-[#222222] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 text-[#00df9a] bg-black ">
+            Get Started
+          </button>
+        </div>
       </div>
     </div>
   );
